1bbd8f59f37c878285c5febb98a23f9004d73011
finn-ev <finn.evermann@leonex.de>
2020-08-04 17:21:21 +0200

    init

---
+++ cookies.js
@@ -0,0 +1,53 @@
+var body = document.body;
+var acceptAllButton = document.querySelector("#acceptAllButton");
+var openSettingsButton = document.querySelector("#openSettingsButton");
+var declineAllButton = document.querySelector("#declineAll");
+var customBannerContainer = document.querySelector(".cookie-banner-container");
+
+window.addEventListener("cf_init", function () {
+  if (CookieFirst.consent) {
+    // Alle Cookie relevanten Elemente werden entfernt, da der Nutzer die Cookies bereits ausgewählt hat
+    removeCustomCookieBanner();
+  } else {
+    removeCFBanner();
+    showCustomBanner();
+  }
+
+  ///////////////////
+  // helper functions
+
+  // der Standard CookieFirst-Banner wird entfernt
+  function removeCFBanner() {
+    var cfRoot = document.querySelector("#cf-root");
+    var cfBanner = cfRoot.childNodes[0].childNodes[0];
+    cfBanner.style.display = "none";
+  }
+
+  // initial ist der Custom-Banner auf visibility: "hidden" gestellt
+  function showCustomBanner() {
+    customBannerContainer.style.visibility = "visible";
+    body.style.overflow = "hidden";
+  }
+
+  function removeCustomCookieBanner() {
+    customBannerContainer.remove();
+    body.style.overflow = "visible";
+  }
+
+  //////////////////////////////
+  // Reaktionen auf Nutzerauswahl
+  acceptAllButton.addEventListener("click", function () {
+    CookieFirst.acceptAllCategories();
+    removeCustomCookieBanner();
+  });
+
+  declineAllButton.addEventListener("click", function () {
+    CookieFirst.declineAllCategories();
+    removeCustomCookieBanner();
+  });
+
+  openSettingsButton.addEventListener("click", function () {
+    CookieFirst.openPanel();
+    removeCustomCookieBanner();
+  });
+});
